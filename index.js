5226c4b60a8e883897d7b75d06731f5d2a690e2d
kiranraj20 <kiranraj80555@gmail.com>
2025-10-10 12:19:24 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("./src/config/cors");
+const corsMiddleware = require("./src/config/cors");
 const transcodeRouter = require("./src/routes/transcode");
 const logger = require("./src/config/logger");
 
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors());
+app.use(corsMiddleware);
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
